8d2baf2396a670d2144a21831d628cc4216d152d
Ahmed Medhat <ahmed.medhat.rabie@gmail.com>
2022-04-17 13:44:48 +0200

    add guard to grade.erl

--- grade.erl
+++ grade.erl
@@ -7,5 +7,8 @@ grade(10) ->
 grade(9) ->
 	io:format("Very Good~n");
 	
-grade(N) ->
-	io:format("You Got ~p~n", [N]).
+grade(N) when N<10 ->
+	io:format("You Got ~p~n", [N]);
+	
+grade(N) when N>10 ->
+	io:format("~p Is Not Valid Number ~n", [N]).
